12d5c04647563e68f6ac568a604bd6620306b0ce
Shad0w30 <51172311+Shad0w30@users.noreply.github.com>
2026-01-21 20:54:11 +0400

    Delete exporters directory

--- exporters/pdfReport.js
+++
@@ -1,8 +0,0 @@
-export function exportPDF(findings) {
-  const doc = new jspdf.jsPDF();
-  doc.text('Mobile App Security Report', 10, 10);
-  findings.forEach((f, i) => {
-    doc.text(`${i+1}. ${f.title} (${f.severity})`, 10, 20 + i * 10);
-  });
-  doc.save('report.pdf');
-}
